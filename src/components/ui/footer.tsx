08170db43e7430fd94fd59866128da65eec8aa79
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2023-10-20 19:00:36 -0300

    feat:created Footer component

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="mt-8 w-full bg-accent py-5 text-xs">
+      <p className="text-center opacity-50">© 2023 Copyright FSW Store</p>
+    </footer>
+  );
+};
+
+export default Footer;
