f46184481adabcd7db1b3dbd0def6e77717b61e0
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2023-10-20 19:01:17 -0300

    feat: add second and third banner and product list keyboard and mouses

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,6 +3,7 @@ import Categories from "./components/categories";
 import { prismaClient } from "@/lib/prisma";
 import ProductList from "@/components/ui/product-list";
 import SectionTitle from "@/components/ui/section-title";
+import { BannerSection } from "./components/banner-section";
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
@@ -13,14 +14,32 @@ export default async function Home() {
     },
   });
 
+  const keyboards = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "keyboards",
+      },
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  });
+
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "mouses",
+      },
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  });
+
   return (
     <div className="flex flex-col gap-8">
-      <Image
+      <BannerSection
         src="/banner-home-1.png"
-        width={0}
-        height={0}
-        sizes="100vw"
-        className="h-auto w-full px-5"
         alt="Até 55% de desconto esse mês"
       />
 
@@ -31,6 +50,26 @@ export default async function Home() {
       <div className="flex flex-col">
         <SectionTitle>Ofertas</SectionTitle>
         <ProductList products={deals} />
+      </div>
+
+      <BannerSection
+        src="/banner-home-2.png"
+        alt="Até 55% de desconto em mouses"
+      />
+
+      <div>
+        <SectionTitle>Teclados</SectionTitle>
+        <ProductList products={keyboards} />
+      </div>
+
+      <BannerSection
+        src="/banner-home-3.png"
+        alt="Até 20% de desconto em fones"
+      />
+
+      <div>
+        <SectionTitle>Mouses</SectionTitle>
+        <ProductList products={mouses} />
       </div>
     </div>
   );
