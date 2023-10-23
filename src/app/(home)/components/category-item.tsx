4175d12cecdabb0582f140664b19ee082bb644a9
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2023-10-22 22:55:12 -0300

    chore: add badge title in the page catalog product

--- src/app/(home)/components/category-item.tsx
+++ src/app/(home)/components/category-item.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Badge } from "@/components/ui/badge";
+import { CATEGORY_ITEM } from "@/constants/categoryIcon";
 import { Category } from "@prisma/client";
 import {
   HeadphonesIcon,
@@ -10,29 +11,23 @@ import {
   SpeakerIcon,
   SquareIcon,
 } from "lucide-react";
+import Link from "next/link";
 
 interface CategoryItemProps {
   category: Category;
 }
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
-  const categoryIcon = {
-    keyboards: <KeyboardIcon size={16} />,
-    monitors: <MonitorIcon size={16} />,
-    headphones: <HeadphonesIcon size={16} />,
-    mousepads: <SquareIcon size={16} />,
-    speakers: <SpeakerIcon size={16} />,
-    mouses: <MouseIcon size={16} />,
-  };
-
   return (
-    <Badge
-      variant="outline"
-      className="flex justify-center gap-2 rounded-lg py-3"
-    >
-      {categoryIcon[category.slug as keyof typeof categoryIcon]}
-      <span className="text-xs font-bold">{category.name}</span>
-    </Badge>
+    <Link href={`catalog/${category.slug}`}>
+      <Badge
+        variant="outline"
+        className="flex justify-center gap-2 rounded-lg py-3"
+      >
+        {CATEGORY_ITEM[category.slug as keyof typeof CATEGORY_ITEM]}
+        <span className="text-xs font-bold">{category.name}</span>
+      </Badge>
+    </Link>
   );
 };
 
