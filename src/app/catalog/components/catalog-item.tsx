32ffb9ae59b8f81b01a9309427c8ba0190b555dd
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2023-10-22 22:22:16 -0300

    chore:ensure that when clicking on the catalog item you go to the category's product page.

--- src/app/catalog/components/catalog-item.tsx
+++ src/app/catalog/components/catalog-item.tsx
@@ -1,5 +1,6 @@
 import { Category } from "@prisma/client";
 import Image from "next/image";
+import Link from "next/link";
 
 interface CatalogItem {
   category: Category;
@@ -7,21 +8,23 @@ interface CatalogItem {
 
 const CatalogItem = ({ category }: CatalogItem) => {
   return (
-    <div className="flex flex-col overflow-hidden rounded-lg">
-      <div className="bg-gradient-category flex h-36 w-full items-center justify-center">
-        <Image
-          src={category.imageUrl}
-          alt={category.name}
-          width={0}
-          height={0}
-          sizes="100vw"
-          className="h-auto max-h-[70%] w-auto max-w-[80%]"
-        />
+    <Link href={`/catalog/${category.slug}`}>
+      <div className="flex flex-col overflow-hidden rounded-lg">
+        <div className="bg-gradient-category flex h-36 w-full items-center justify-center">
+          <Image
+            src={category.imageUrl}
+            alt={category.name}
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-auto max-h-[70%] w-auto max-w-[80%]"
+          />
+        </div>
+        <div className="bg-accent">
+          <p className="py-3 text-center text-sm">{category.name}</p>
+        </div>
       </div>
-      <div className="bg-accent">
-        <p className="py-3 text-center text-sm">{category.name}</p>
-      </div>
-    </div>
+    </Link>
   );
 };
 
--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,10 +1,10 @@
 import { Badge } from "@/components/ui/badge";
 import { LayoutGridIcon } from "lucide-react";
-import CategoryList from "./components/catalog-list";
+import CatalogList from "./components/catalog-list";
 
 const CategoryPage = () => {
   return (
-    <div className="flex flex-col gap-8 pt-8 pb-16">
+    <div className="flex flex-col gap-8 pb-16 pt-8">
       <div className="pl-5">
         <Badge
           variant="outline"
@@ -14,7 +14,7 @@ const CategoryPage = () => {
           Catálogo
         </Badge>
       </div>
-      <CategoryList />
+      <CatalogList />
     </div>
   );
 };
