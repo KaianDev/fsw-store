756c2c2edcd1ef4e66b414027689473bad0b098a
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2023-10-27 13:51:58 -0300

    fix: adjustment in the ProductDetails Components

--- src/app/product/[slug]/components/product-details.tsx
+++ src/app/product/[slug]/components/product-details.tsx
@@ -3,25 +3,41 @@
 import { Badge } from "@/components/ui/badge";
 import { ProductWithTotalPriceInterface } from "@/helpers/computed-total-price";
 import { localeToBrlPrice } from "@/helpers/locale-to-brl-price";
-import { ArrowDownIcon, TruckIcon } from "lucide-react";
-import QuantityControl from "./quantity-control";
+import {
+  ArrowDownIcon,
+  ChevronLeft,
+  ChevronRight,
+  TruckIcon,
+} from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 interface ProductDetailsProps {
   product: ProductWithTotalPriceInterface;
 }
 const ProductDetails = ({ product }: ProductDetailsProps) => {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleDecreaseQuantity = () => {
+    setQuantity((prev) => (prev === 1 ? prev : prev - 1));
+  };
+
+  const handleIncreaseQuantity = () => {
+    setQuantity((prev) => prev + 1);
+  }
+
   return (
     <div className="flex flex-col gap-8 px-5">
+      
       <div className="flex flex-col gap-4 ">
-        <p className="text-lg">{product.name}</p>
+        <h2 className="text-lg">{product.name}</h2>
         <div>
           {product.discountPercentage > 0 ? (
             <>
               <div className="flex items-center gap-2 ">
-                <p className="text-2xl font-bold">
+                <h1 className="text-2xl font-bold">
                   {localeToBrlPrice(product.totalPrice)}
-                </p>
+                </h1>
                 <Badge className="flex w-fit px-2 hover:bg-primary">
                   <ArrowDownIcon size={16} />
                   <span className="text-xs font-bold">
@@ -42,13 +58,33 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
             </p>
           )}
         </div>
-        <QuantityControl />
+
+        <div className="flex flex-row items-center">
+          <Button
+            variant="outline"
+            className="px-2"
+            onClick={handleDecreaseQuantity}
+          >
+            <ChevronLeft />
+          </Button>
+          <div className="w-8 text-center">{quantity}</div>
+          <Button
+            variant="outline"
+            className="px-2"
+            onClick={handleIncreaseQuantity}
+          >
+            <ChevronRight />
+          </Button>
+        </div>
       </div>
+
       <div>
-        <h2 className="text mb-2 font-bold">Descrição</h2>
-        <p>{product.description}</p>
+        <h3 className="mb-3 font-bold">Descrição</h3>
+        <p className="text-sm opacity-75 text-justify">{product.description}</p>
       </div>
+
       <Button className="font-bold uppercase">Adicionar ao carrinho</Button>
+
       <div className="flex items-center justify-between rounded-lg bg-accent px-5 py-2">
         <div className="flex items-center gap-2">
           <TruckIcon size={24} />
@@ -63,6 +99,7 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
         </div>
         <p className="text-sm font-bold">Frete grátis</p>
       </div>
+
     </div>
   );
 };
